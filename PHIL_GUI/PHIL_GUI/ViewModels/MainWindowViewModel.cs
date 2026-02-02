b593dab9e036261d72e7b9070ec5288ad1974505
Catalin Chiprian <chiprianvcatalin@gmail.com>
2026-02-02 15:58:37 +0100

    Refactor command properties to use private RelayCommand fields

--- PHIL_GUI/PHIL_GUI/ViewModels/MainWindowViewModel.cs
+++ PHIL_GUI/PHIL_GUI/ViewModels/MainWindowViewModel.cs
@@ -34,7 +34,7 @@ public class MainWindowViewModel : ViewModelBase
         {
             if (SetProperty(ref _selectedPort, value))
             {
-                (ConnectCommand as RelayCommand)?.RaiseCanExecuteChanged();
+                _connectCommand.RaiseCanExecuteChanged();
             }
         }
     }
@@ -58,7 +58,7 @@ public class MainWindowViewModel : ViewModelBase
         {
             if (SetProperty(ref _messageToSend, value))
             {
-                (SendMessageCommand as RelayCommand)?.RaiseCanExecuteChanged();
+                _sendMessageCommand.RaiseCanExecuteChanged();
             }
         }
     }
@@ -67,33 +67,32 @@ public class MainWindowViewModel : ViewModelBase
         get => _currentPage;
         set => SetProperty(ref _currentPage, value);
     }
-    
-    public ICommand GetPortsCommand { get; }
-    public ICommand ConnectCommand { get; }
-    public ICommand DisconnectCommand { get; }
-    public ICommand SendMessageCommand { get; }
+
+    private RelayCommand _connectCommand;
+    private RelayCommand _disconnectCommand;
+    private RelayCommand _sendMessageCommand;
+    private RelayCommand _goToBasicControlsPageCommand;
+    public ICommand ConnectCommand => _connectCommand;
+    public ICommand DisconnectCommand => _disconnectCommand;
+    public ICommand SendMessageCommand => _sendMessageCommand;
+    public ICommand GoToBasicControlsPageCommand => _goToBasicControlsPageCommand;
     public ICommand ClearMonitorCommand { get; }
-    
-    public ICommand GoToBasicControlsPageCommand { get; } 
-    
     public ICommand EmergencyStopCommand { get; }
-    
-    
-    
+    public ICommand GetPortsCommand { get; }
+
     public MainWindowViewModel()
     {
         _serialPortService = new SerialPortService();
         _availablePorts = new ObservableCollection<string>();
-        
+
+        _connectCommand = new RelayCommand(ConnectToPort, CanConnect);
+        _disconnectCommand = new RelayCommand(DisconnectFromPort, CanDisconnect);
+        _sendMessageCommand = new RelayCommand(SendMessage, CanSendMessage);
+        _goToBasicControlsPageCommand = new RelayCommand(GoToBasicControlsPage, CanGoToBasicControlsPage);
+
         EmergencyStopCommand = new RelayCommand(() => SendMotorCommand("s"));
         GetPortsCommand = new RelayCommand(GetAvailablePorts);
-        ConnectCommand = new RelayCommand(ConnectToPort, CanConnect);
-        DisconnectCommand = new RelayCommand(DisconnectFromPort, CanDisconnect);
-        SendMessageCommand = new RelayCommand(SendMessage, CanSendMessage);
         ClearMonitorCommand = new RelayCommand(ClearMonitor);
-        GoToBasicControlsPageCommand = new RelayCommand(GoToBasicControlsPage, CanGoToBasicControlsPage);
-        
-            
         
         ConnectionStatus = "Disconnected";
         ReceivedData = "";
@@ -127,11 +126,11 @@ public class MainWindowViewModel : ViewModelBase
         {
             _serialPortService.Connect(SelectedPort);
             ConnectionStatus = $"Connected to {SelectedPort}";
-            
-            (ConnectCommand as RelayCommand)?.RaiseCanExecuteChanged();
-            (DisconnectCommand as RelayCommand)?.RaiseCanExecuteChanged();
-            (SendMessageCommand as RelayCommand)?.RaiseCanExecuteChanged();
-            (GoToBasicControlsPageCommand as RelayCommand)?.RaiseCanExecuteChanged();
+
+            _connectCommand.RaiseCanExecuteChanged();
+            _disconnectCommand.RaiseCanExecuteChanged();
+            _sendMessageCommand.RaiseCanExecuteChanged();
+            _goToBasicControlsPageCommand.RaiseCanExecuteChanged();
         }
         catch (Exception ex)
         {
@@ -144,11 +143,10 @@ public class MainWindowViewModel : ViewModelBase
         _serialPortService.Disconnect();
         ConnectionStatus = "Disconnected";
         
-        (ConnectCommand as RelayCommand)?.RaiseCanExecuteChanged();
-        (DisconnectCommand as RelayCommand)?.RaiseCanExecuteChanged();
-        (SendMessageCommand as RelayCommand)?.RaiseCanExecuteChanged();
-        (GoToBasicControlsPageCommand as RelayCommand)?.RaiseCanExecuteChanged(); 
-        
+        _connectCommand.RaiseCanExecuteChanged();
+        _disconnectCommand.RaiseCanExecuteChanged();
+        _sendMessageCommand.RaiseCanExecuteChanged();
+        _goToBasicControlsPageCommand.RaiseCanExecuteChanged();      
     }
     
     private void OnMessageReceived(string message)
@@ -177,8 +175,8 @@ public class MainWindowViewModel : ViewModelBase
             });
 
             MessageToSend = "";
-            
-            (SendMessageCommand as RelayCommand)?.RaiseCanExecuteChanged();
+
+            _sendMessageCommand.RaiseCanExecuteChanged();
         }
     }
     
