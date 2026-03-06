1020bbed0097adfdebe1e4e1a133a03cd01c8b01
Catalin Chiprian <chiprianvcatalin@gmail.com>
2026-03-06 16:20:46 +0100

    Add bottom bar

--- PHIL_GUI/PHIL_GUI/ViewModels/WellsViewModel.cs
+++ PHIL_GUI/PHIL_GUI/ViewModels/WellsViewModel.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Mvvm.Input;
 using PHIL_GUI.ViewModels.Base;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using RelayCommand = PHIL_GUI.Commands.RelayCommand;
 
@@ -12,6 +13,14 @@ public class WellsViewModel : CommunicationBase
     public ICommand WellsPositionCommand { get; }
     public ICommand MoveUpCommand { get; }
     public ICommand MoveDownCommand { get; }
+
+    public ObservableCollection<string> Wells { get; } = new();
+    public int WellsCount { get; set; } = 12;
+
+    public string RmsL;
+    public string RmsR;
+    public int RmsCount { get; set; }
+    public string Microsteps;
     
     public WellsViewModel()
     {
@@ -20,5 +29,15 @@ public class WellsViewModel : CommunicationBase
         MoveUpCommand = new RelayCommand(() => Send("u"));
         MoveDownCommand = new RelayCommand(() => Send("d"));
         WellsPositionCommand = new RelayCommand<string>(w => Send($"w{w?.ToLower()}"));
+
+        var rows = new[] { "A", "B", "C", "D", "E", "F", "G", "H" };
+
+        foreach (var row in rows)
+        {
+            for (int col = 1; col <= WellsCount; col++)
+            {
+                Wells.Add($"{row}{col}");
+            }
+        }
     }
 }
