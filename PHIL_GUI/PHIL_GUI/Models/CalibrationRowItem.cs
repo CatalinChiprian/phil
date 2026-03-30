865ce5a989125f8ed2967462d6af2527c74b5454
Catalin Chiprian <chiprianvcatalin@gmail.com>
2026-03-30 13:35:13 +0200

    Add Calibration DataTable

---
+++ PHIL_GUI/PHIL_GUI/Models/CalibrationRowItem.cs
@@ -0,0 +1,18 @@
+﻿namespace PHIL_GUI.Models
+{
+    public class CalibrationRowItem
+    {
+        public string Well { get; set; }
+        public string XY { get; set; }
+        public float ErrL { get; set; }
+        public float ErrR { get; set; }
+
+        public CalibrationRowItem(string well, string xy, float errL, float errR)
+        {
+            Well = well;
+            XY = xy;
+            ErrL = errL;
+            ErrR = errR;
+        }
+    }
+}
--- PHIL_GUI/PHIL_GUI/ViewModels/CalibrationViewModel.cs
+++ PHIL_GUI/PHIL_GUI/ViewModels/CalibrationViewModel.cs
@@ -1,9 +1,21 @@
-﻿using PHIL_GUI.ViewModels.Base;
+﻿using PHIL_GUI.Models;
+using PHIL_GUI.ViewModels.Base;
+using System.Collections.ObjectModel;
 
 namespace PHIL_GUI.ViewModels
 {
     public class CalibrationViewModel : ViewModelBase
     {
+        public ObservableCollection<CalibrationRowItem> CalibrationRows { get; } = new()
+        {
+            new CalibrationRowItem("A1", "0, 36", 0.5f, 0.8f),
+            new CalibrationRowItem("B2", "45, 63", 0.3f, 0.6f),
+            new CalibrationRowItem("C3", "18, 0", 0.7f, 0.9f)
+        };
 
+        public CalibrationViewModel()
+        {
+            CalibrationRows.Add(new CalibrationRowItem("D4", "90, 90", 0.2f, 0.4f));
+        }
     }
 }
