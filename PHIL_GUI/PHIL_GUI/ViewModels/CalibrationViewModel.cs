19d4cf6d481a79338e8c0a703d8dcbc9287b6211
Catalin Chiprian <chiprianvcatalin@gmail.com>
2026-04-02 10:43:42 +0200

    Remove CalibrationRows

--- PHIL_GUI/PHIL_GUI/ViewModels/CalibrationViewModel.cs
+++ PHIL_GUI/PHIL_GUI/ViewModels/CalibrationViewModel.cs
@@ -28,28 +28,6 @@ namespace PHIL_GUI.ViewModels
 
         public WellPlateItem WellPlate { get; } = new WellPlateItem(true);
 
-        // Used for Front End testing, not actual calibration points
-        public ObservableCollection<CalibrationRowItem> CalibrationRows { get; } = new()
-        {
-            new CalibrationRowItem("A1", "0, 36", 0.5f, 0.8f),
-            new CalibrationRowItem("B2", "45, 63", 0.3f, 0.6f),
-            new CalibrationRowItem("C3", "18, 0", 0.7f, 0.9f),
-            new CalibrationRowItem("D4", "90, 90", 0.2f, 0.4f),
-            new CalibrationRowItem("E5", "30, 45", 0.4f, 0.7f),
-            new CalibrationRowItem("F6", "60, 30", 0.6f, 0.8f),
-            new CalibrationRowItem("G7", "15, 75", 0.5f, 0.9f),
-            new CalibrationRowItem("H8", "75, 15", 0.3f, 0.5f),
-            new CalibrationRowItem("I9", "90, 0", 0.4f, 0.6f),
-            new CalibrationRowItem("J10", "0, 90", 0.2f, 0.4f),
-            new CalibrationRowItem("K11", "45, 45", 0.5f, 0.7f),
-            new CalibrationRowItem("L12", "30, 60", 0.6f, 0.8f),
-            new CalibrationRowItem("M13", "60, 30", 0.4f, 0.6f),
-            new CalibrationRowItem("N14", "15, 75", 0.5f, 0.9f),
-            new CalibrationRowItem("O15", "75, 15", 0.3f, 0.5f),
-            new CalibrationRowItem("P16", "90, 0", 0.4f, 0.6f),
-            new CalibrationRowItem("Q17", "0, 90", 2f, 2f),
-        };
-
         public CalibrationViewModel()
         {
             MoveForwardCommand = new RelayCommand(RobotProtocol.MoveForward);
@@ -65,7 +43,7 @@ namespace PHIL_GUI.ViewModels
             Calibration.Points.CollectionChanged += Points_CollectionChanged; ;
         }
 
-        private void Points_CollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        private void Points_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
         {
             OnPropertyChanged(nameof(SolveEnabled));
             if (e.Action == NotifyCollectionChangedAction.Add)
