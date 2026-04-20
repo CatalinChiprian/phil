7ad09cbf7d26c1d5f7aa741440df416a29cdc303
Catalin Chiprian <chiprianvcatalin@gmail.com>
2026-04-20 15:01:47 +0200

    Add new ControlsView

---
+++ PHIL_GUI/PHIL_GUI/ViewModels/SettingsWindow/ViewModels/ControlsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using PHIL_GUI.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHIL_GUI.ViewModels
+{
+    public class ControlsViewModel : ViewModelBase
+    {
+
+    }
+}
